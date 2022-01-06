ea495fe273fb70782bd9257fc7e2f9ec410aedda
James Edwards <90045447+edwardjimi@users.noreply.github.com>
2022-01-06 20:40:35 +0000

    Add files via upload

---
+++ Block Letters ASCII Art.py
@@ -0,0 +1,19 @@
+print("JJJJJ EEEEE")
+print("  J   E    ")
+print("  J   E    ")
+print("  J   EEE  ")
+print("J J   E    ")
+print("J J   E    ")
+print(" J    EEEEE")
+
+name = """
+JJJJJ EEEEE
+  J   E
+  J   E   
+  J   EEE
+J J   E
+J J   E
+ J    EEEEE
+"""
+
+print(name)
